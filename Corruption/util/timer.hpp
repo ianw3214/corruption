1c74cac7587b4cd665ebc0c10b22492a5500af10
Ian <ianw3214@gmail.com>
2019-11-26 23:34:11 -0500

    Add timer class

---
+++ Corruption/util/timer.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <chrono>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+class Timer
+{
+public:
+    Timer(const char * name) : m_name(name), m_stopped(false)
+    {
+        m_startTime = std::chrono::high_resolution_clock::now();
+    }
+
+    ~Timer()
+    {
+        if (!m_stopped)
+        {
+            Stop();
+        }
+    }
+
+    void Stop()
+    {
+        auto endTime = std::chrono::high_resolution_clock::now();
+
+        long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_startTime).time_since_epoch().count();
+        long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTime).time_since_epoch().count();
+
+        float duration = (end - start) * 0.001f;
+
+        m_stopped = true;
+    }
+
+private:
+    const char * m_name;
+    bool m_stopped;
+
+    std::chrono::time_point<std::chrono::steady_clock> m_startTime;
+};
